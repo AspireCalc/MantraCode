bbd01d3b9d614fcc0dac5c2c5e0ec73d68926989
Nishant Chauhan <formystudy11230@gmail.com>
2026-06-07 21:11:04 +0530

    01: Project setup and Component architecture

---
+++ packages/cli/src/components/header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+        <box justifyContent="center" alignItems="center">
+            <box flexDirection="row" justifyContent="center" gap={0.5} alignItems="center">
+                <ascii-font font="tiny" text="Mantra" color="#E5E5EA" />
+                <ascii-font font="tiny" text="Code" color={"#FF651D"} />
+            </box>
+        </box>
+    )
+};
